632af9be058fd4bd74e8dbc97f20bb0ad053b876
10cheon00 <10cheon00@gmail.com>
2024-02-09 13:05:31 +0900

    로그인 구현을 위한 초기화

--- src/main/java/com/learn/security/SecurityApplication.java
+++ src/main/java/com/learn/security/SecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityToyProjectApplication {
+public class SecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityToyProjectApplication.class, args);
+		SpringApplication.run(SecurityApplication.class, args);
 	}
 
 }
